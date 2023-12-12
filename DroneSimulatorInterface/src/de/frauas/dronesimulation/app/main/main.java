ede5066795256ead557f335d0328a29aa811bace
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2023-12-12 19:22:43 +0100

    Added API testing in main method

--- DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
@@ -1,9 +1,23 @@
 package de.frauas.dronesimulation.app.main;
-public class main {
 
+import de.frauas.dronesimulation.app.apiconnection.apihandler; // Import the apihandler class
+import java.util.List; // Add this line at the top of your file
+
+public class main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create an instance of apihandler
+		apihandler handler = new apihandler();
+
+		// Call the Drone Dynamics API with specific id which is same id as Drone List
+		// instance id and uri.
+		apihandler.callDroneDynamics("http://dronesim.facets-labs.com/api/drones/55/");
+
+		// Call the Drone List API to get a list of drones, retrieving the first 20
+		// drones
+		apihandler.callDroneListAPI(0, 20);
+
+		// Call the Drone Type API for the drone type with spicific id
+		apihandler.callDroneTypeAPI("http://dronesim.facets-labs.com/api/dronetypes/65/");
 
 	}
-
 }
