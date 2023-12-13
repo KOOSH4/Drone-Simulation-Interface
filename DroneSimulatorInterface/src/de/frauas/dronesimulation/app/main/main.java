39ab812278d3784b0ae1867412d1cbd9e42dfca0
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2023-12-13 17:12:02 +0100

    add some comments and deleted unnecessary comments

--- DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
@@ -1,23 +1,28 @@
 package de.frauas.dronesimulation.app.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.frauas.dronesimulation.app.apiconnection.apihandler; // Import the apihandler class
-import java.util.List; // Add this line at the top of your file
+import de.frauas.dronesimulation.app.dronelist.DroneList;
 
 public class main {
 	public static void main(String[] args) {
-		// Create an instance of apihandler
-		apihandler handler = new apihandler();
 
-		// Call the Drone Dynamics API with specific id which is same id as Drone List
-		// instance id and uri.
-		apihandler.callDroneDynamics("http://dronesim.facets-labs.com/api/drones/55/");
+		List<DroneList> droneInstanceList = new ArrayList<>(); // Initialize empty list for DroneList
+		apihandler.callDroneListAPI(0, 20, droneInstanceList); // offset , Limit
 
-		// Call the Drone List API to get a list of drones, retrieving the first 20
-		// drones
-		apihandler.callDroneListAPI(0, 20);
+		for (DroneList droneinstance : droneInstanceList) {
 
-		// Call the Drone Type API for the drone type with spicific id
-		apihandler.callDroneTypeAPI("http://dronesim.facets-labs.com/api/dronetypes/65/");
+			apihandler.callDroneTypeAPI(droneinstance); // get and write drone type for each drone in list.
+			droneinstance.printStatus(); // print drone instance status
+			droneinstance.getDroneType().printStatus(); // print drone type status
 
+			System.out.println("--------------------------");
+		}
+
+		apihandler.callDroneDynamics(droneInstanceList.get(1)); // get amd write drone dynamics for second drone in
+																// list.
+		droneInstanceList.get(1).getDroneDynamicsList().get(0).printStatus(); // print drone dynamics status
 	}
 }
