5e41ee1aab07a995ea631e467a7d39b209ef5431
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2023-12-13 16:31:12 +0100

    created new parsing methode for each class that create objects

---
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/dronedynamics/ParseDroneDynamics.java
@@ -0,0 +1,52 @@
+package de.frauas.dronesimulation.app.dronedynamics;
+
+import org.json.JSONObject;
+
+import de.frauas.dronesimulation.app.dronelist.DroneList;
+
+import org.json.JSONArray;
+import java.time.ZonedDateTime;
+
+public class ParseDroneDynamics {
+    public static void parseJsonResponse(String input, DroneList drone) {
+        JSONObject wholeFile = new JSONObject(input);
+        int DroneId = drone.getId();
+        String DroneUri = "http://dronesim.facets-labs.com/api/drones/" + DroneId + "/?format=json";
+        JSONArray jsonFileDynamic = wholeFile.getJSONArray("results");
+        // Loop through the array
+        for (int i = 0; i < jsonFileDynamic.length(); i++) {
+            // Get each object in the array
+            JSONObject object = jsonFileDynamic.getJSONObject(i);
+            // If the object has "carriage_type" and "carriage_weight"
+            // System.out.println("DD URI:" + object.getString("drone"));
+            // System.out.println("Drone URI i created:" + DroneUri);
+
+            if (object.has("speed") && object.getString("drone").equals(DroneUri)) {
+                // Get the values of "carriage_type" and "carriage_weight"
+
+                String droneLink = object.getString("drone");
+                ZonedDateTime timestamp = ZonedDateTime.parse(object.getString("timestamp"));
+                int speed = object.getInt("speed");
+                String alignRoll = object.getString("align_roll");
+                String alignPitch = object.getString("align_pitch");
+                String alignYaw = object.getString("align_roll");
+                String longitude = object.getString("longitude");
+                String latitude = object.getString("latitude");
+                int batteryStatus = object.getInt("battery_status");
+                ZonedDateTime lastSeen = ZonedDateTime.parse(object.getString("last_seen"));
+                String status = object.getString("status");
+
+                // Print the values
+                System.out.println("Drone Dynamics: ");
+                System.out.println(
+                        "\n" + status + " " + droneLink + " " + timestamp + " " + speed + " " + alignRoll
+                                + " " + alignPitch + " " + alignYaw + " " + longitude + " " + latitude + " "
+                                + batteryStatus + " " + lastSeen + "\n");
+                DroneDynamics droneDynamics = new DroneDynamics(droneLink, timestamp, speed, alignRoll, alignPitch,
+                        alignYaw, longitude, latitude, batteryStatus, lastSeen, status);
+                drone.addDroneDynamics(droneDynamics);
+                System.out.println("The DroneDynamics object is created.");
+            }
+        }
+    }
+}
---
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/dronetype/ParseDroneType.java
@@ -0,0 +1,28 @@
+package de.frauas.dronesimulation.app.dronetype;
+
+import org.json.JSONObject;
+
+import de.frauas.dronesimulation.app.dronelist.DroneList;
+
+public class ParseDroneType {
+    public static void parseJsonResponse(String input, DroneList drone) {
+        JSONObject o = new JSONObject(input);
+        int typeId = o.getInt("id");
+        String manufacturer = o.getString("manufacturer");
+        String typeName = o.getString("typename");
+        int weight = o.getInt("weight");
+        int maxSpeed = o.getInt("max_speed");
+        int batteryCapacity = o.getInt("battery_capacity");
+        int controlRange = o.getInt("control_range");
+        int maxCarriage = o.getInt("max_carriage");
+
+        System.out.println(
+                "\n" + "Drone " + typeId + ": Manufacturer " + manufacturer + " Type name: " + typeName + " "
+                        + "Weight: " + weight + " Max speed: " + maxSpeed + "Km/h)" + " Battery capacity: "
+                        + batteryCapacity + " Control range: " + controlRange + "Max carriage:" + maxCarriage
+                        + "g" + "\n");
+        DroneType dronetype = new DroneType(typeId, manufacturer, typeName, weight, maxSpeed,
+                batteryCapacity, controlRange, maxCarriage);
+        drone.setDroneType(dronetype);
+    }
+}
