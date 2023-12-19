02c8ca60da6235f282d5e24620fdef9a5648a042
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2023-12-19 13:07:31 +0100

    Added getDroneList and getDroneType Helper Methode

--- DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/Helper.java
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/Helper.java
@@ -6,16 +6,15 @@ import de.frauas.dronesimulation.app.dronelist.DroneList;
 
 public class Helper {
 
-	public static void printDroneDynamicsStatus(List<DroneList> droneInstanceList, int count) {
-		droneInstanceList.get(count).getDroneDynamicsList().get(0).printStatus();
+	public static void getDroneList(apihandler apiHandler, int offset, int limit, List<DroneList> droneInstanceList) {
+		apiHandler.callDroneListAPI(offset, limit, droneInstanceList);
 	}
 
-	public static void getDroneDynammicData(apihandler apiHandler, DroneList droneInstance, int minutesBefore) {
-		int offset = 28800 - (minutesBefore * 20);
-		if (offset >= 0) {
-			apiHandler.callDroneDynamics(droneInstance, offset);
-		} else {
-			System.out.println("Invalid minutesBefore value. It should be less than or equal to 1440 (24 hours).");
-		}
+	public static void getDroneType(apihandler apiHandler, DroneList droneInstance) {
+		apiHandler.callDroneTypeAPI(droneInstance);
+	}
+
+	public static void printDroneDynamicsStatus(List<DroneList> droneInstanceList, int count) {
+		droneInstanceList.get(count).getDroneDynamicsList().get(0).printStatus();
 	}
 }
--- DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
@@ -6,23 +6,26 @@ import java.util.List;
 import de.frauas.dronesimulation.app.apiconnection.apihandler; // Import the apihandler class
 import de.frauas.dronesimulation.app.dronelist.DroneList;
 
+import de.frauas.dronesimulation.app.main.Helper;
+
 public class main {
 	public static void main(String[] args) {
-
+		apihandler apihandler = new apihandler();
 		List<DroneList> droneInstanceList = new ArrayList<>(); // Initialize empty list for DroneList
-		apihandler.callDroneListAPI(0, 20, droneInstanceList); // offset , Limit
+		Helper.getDroneList(apihandler, 0, 20, droneInstanceList); // get and write drone list
+		// apihandler.callDroneListAPI(0, 20, droneInstanceList); // offset , Limit
 
 		for (DroneList droneinstance : droneInstanceList) {
-
-			apihandler.callDroneTypeAPI(droneinstance); // get and write drone type for each drone in list.
+			Helper.getDroneType(apihandler, droneinstance);
+			// apihandler.callDroneTypeAPI(droneinstance); // get and write drone type for
+			// each drone in list.
 			droneinstance.printStatus(); // print drone instance status
 			droneinstance.getDroneType().printStatus(); // print drone type status
 
 			System.out.println("--------------------------");
 		}
+		int count = 0;
 
-		apihandler.callDroneDynamics(droneInstanceList.get(1)); // get amd write drone dynamics for second drone in
-																// list.
-		droneInstanceList.get(1).getDroneDynamicsList().get(0).printStatus(); // print drone dynamics status
 	}
+
 }
