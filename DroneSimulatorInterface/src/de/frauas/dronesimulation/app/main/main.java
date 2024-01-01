614a40772eb9beb88a808f151918148186bacd04
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2024-01-01 15:05:59 +0100

    remove Dronelist/type helper function

--- DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/main.java
@@ -12,30 +12,29 @@ public class main {
 	public static void main(String[] args) {
 		apihandler apihandler = new apihandler();
 		List<DroneList> droneInstanceList = new ArrayList<>(); // Initialize empty list for DroneList
-		Helper.getDroneList(apihandler, 0, 20, droneInstanceList); // get and write drone list
-		// apihandler.callDroneListAPI(0, 20, droneInstanceList); // offset , Limit
+		apihandler.callDroneListAPI(0, 20, droneInstanceList);
 
 		for (DroneList droneinstance : droneInstanceList) {
-			Helper.getDroneType(apihandler, droneinstance);
-			// apihandler.callDroneTypeAPI(droneinstance); // get and write drone type for
+			apihandler.callDroneTypeAPI(droneinstance); // get and write drone type for
 			// each drone in list.
 			droneinstance.printStatus(); // print drone instance status
 			droneinstance.getDroneType().printStatus(); // print drone type status
 
 			System.out.println("--------------------------");
 		}
-		int count = 0;
+		int count = 1;
 
-		Helper.getDroneDynammics(apihandler, droneInstanceList.get(count), 0); // get amd write drone dynamics for
-																				// drone
-																				// dynamics for
-																				// second drone
-																				// in
+		Helper.getDroneDynammics(apihandler, droneInstanceList.get(count), 100); // get amd write drone dynamics for
+																					// drone
+																					// dynamics for
+																					// second drone
+																					// in
 		// apihandler.callDroneDynamics(droneInstanceList.get(count), 12); // get amd
 		// write drone dynamics for second drone
 		// in
 		// list.
 		Helper.printDroneDynamicsStatus(droneInstanceList, count);
+		Helper.payloadPercentage(droneInstanceList.get(count));
 		// droneInstanceList.get(count).getDroneDynamicsList().get(0).printStatus(); //
 		// print drone dynamics status
 	}
