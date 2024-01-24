777a238fea86bae3660ee36573afcf5bd5803dbd
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2024-01-24 14:12:59 +0100

    Add Error handling fro helper

--- DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/Helper.java
+++ DroneSimulatorInterface/src/de/frauas/dronesimulation/app/main/Helper.java
@@ -1,11 +1,39 @@
 package de.frauas.dronesimulation.app.main;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.XMLFormatter;
+import java.io.IOException;
 import java.util.List;
 import de.frauas.dronesimulation.app.apiconnection.ApiHandler; // Import the apihandler class
 import de.frauas.dronesimulation.app.dronedynamics.DroneDynamics;
 import de.frauas.dronesimulation.app.dronelist.DroneList;
 
 public class Helper {
+
+	private static final Logger LOG = Logger.getGlobal();
+
+	// Static block for setting up logger handlers
+	static {
+		Handler fileHandler;
+		Handler consoleHandler;
+		try {
+			// File handler for logging to a file
+			fileHandler = new FileHandler("./HelperLogFile.log");
+			LOG.addHandler(fileHandler);
+			Formatter xmlFormat = new XMLFormatter();
+			fileHandler.setFormatter(xmlFormat);
+			fileHandler.setLevel(Level.ALL);
+		} catch (IOException e) {
+			LOG.severe("An error occurred while setting up the logger handlers: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 	public static void getDroneDynamics(ApiHandler droneApiHandler, List<DroneList> listOfDrones, int minutesBefore,
 			List<DroneDynamics> listOfDronesDynamicTimeStamp) {
@@ -15,10 +43,10 @@ public class Helper {
 				droneApiHandler.fetchDroneDynamics(listOfDrones, offset, listOfDronesDynamicTimeStamp);
 				listOfDronesDynamicTimeStamp.add(listOfDrones.get(0).getDroneDynamics());
 			} else {
-				System.out.println("Invalid minutesBefore value. It should be less than or equal to 1440 (24 hours).");
+				System.out.println("Invalid minutesBefore value.");
 			}
 		} catch (Exception e) {
-			System.out.println("An error occurred while getting drone dynamics: " + e.getMessage());
+			LOG.warning("An error occurred while fetching dynamics info:  " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
@@ -27,7 +55,7 @@ public class Helper {
 		try {
 			listOfDrones.get(selectedDroneIndex).getDroneDynamicsList().get(0).printStatus();
 		} catch (Exception e) {
-			System.out.println("An error occurred while printing drone dynamics status: " + e.getMessage());
+			LOG.warning("An error occurred while fetching dynamics info:  " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
@@ -39,7 +67,7 @@ public class Helper {
 			System.out.println("PAYLOAD PERCENTAGE: " + payloadPercentage);
 			return payloadPercentage;
 		} catch (Exception e) {
-			System.out.println("An error occurred while calculating payload percentage: " + e.getMessage());
+			LOG.warning("An error occurred while fetching dynamics info:  " + e.getMessage());
 			e.printStackTrace();
 			return 0;
 		}
